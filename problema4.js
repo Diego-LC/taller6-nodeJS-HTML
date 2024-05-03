0f2472af2cd37fed1db809795b0633cc08438ac4
DIΣGӨ L <di3golabrin@gmail.com>
2024-05-03 10:53:07 -0400

    problema 1

---
+++ problema4.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+// Configuración del motor de plantillas EJS
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(express.urlencoded({ extended: false }));
+
+// Ruta para renderizar la página con la tabla de colores
+app.get('/', (req, res) => {
+    idioma = 'esp';
+    res.render('idioma_in', {idioma});
+});
+
+app.post('/procesar', (req, res) => {
+    const idioma = req.body.idioma;
+    console.log(m, n);
+    res.render('idioma_out', { idioma});
+});
+
+app.listen(3055, () => {
+    console.log('Servidor web en ejecución en el puerto 3055');
+});
